10dfb277a16b60f0e79fe51c43699babea08e28f
Sarab <sarabjothbhatia@gmail.com>
2025-04-13 12:12:23 +0530

    added footer, sidebar cleanup and svg files

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import Sidebar from '../components/sidebar';
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/sidebar';
+import Footer from '../components/Footer' // Assuming you have one
 
 const MainLayout = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex min-h-screen bg-gray-100 text-black">
+      {/* Sidebar */}
       <Sidebar />
-      <main className="flex-1 overflow-y-auto">
-        <Outlet />
-      </main>
+
+      {/* Page Content */}
+      <div className="flex-1 flex flex-col">
+        <main className="flex-1 p-6 overflow-y-auto">
+          <Outlet /> {/* This is where your Dashboard/Home/Help will load */}
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
     </div>
   );
 };
 
-export default MainLayout;
+export default MainLayout;
