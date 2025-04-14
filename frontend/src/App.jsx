651fb5d5af0e97c2919713e4bbea2843f947077d
Samarth Tanay <samarth27tanay17122004@gmail.com>
2025-04-14 22:24:13 +0530

    Added transactiontable, bankswitcher components and transactionhistory page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -42,6 +42,7 @@ import Dashboard from './pages/Dashboard'
 import LinkBank from './pages/LinkBank'
 import Help from "./pages/Help"
 import MainLayout from './layout/MainLayout'
+import TransactionHistory from './pages/TransactionHistory'
 import Home from "./pages/Home"
 import AboutUs from "./pages/AboutUs"  // Import the new AboutUs component
 
@@ -59,8 +60,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />  {/* Added AboutUs route */}
         <Route path="/my-banks" element={<Navigate to="/dashboard" replace />} />  {/* Redirect to dashboard for now */}
-        <Route path="/transaction-history" element={<Navigate to="/dashboard" replace />} />  {/* Redirect to dashboard for now */}
-        
+                
         {/* Redirect /dashboard to /home if you want Home as the default */}
         <Route path="/" element={<Navigate to="/home" replace />} />
       </Route>
