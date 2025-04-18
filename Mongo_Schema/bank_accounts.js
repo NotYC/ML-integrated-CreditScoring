318b23fb88127f1721c58b8815d3a6372cdb390e
gautam343 <gautam.sharma22@st.niituniversity.in>
2025-04-19 00:42:12 +0530

    Schema: Bank Account associated with KYCS

---
+++ Mongo_Schema/bank_accounts.js
@@ -0,0 +1,10 @@
+{
+    _id: ObjectId("..."),           // bankId
+    user_id: ObjectId("..."),       // foreign key to users._id
+    bankName: "ICICI Bank",
+    accountNumber: "XXXX1234",
+    ifsc: "ICIC0001234",
+    accountType: "Savings",
+    addedAt: ISODate("2025-04-19T13:00:00Z")
+  }
+  
