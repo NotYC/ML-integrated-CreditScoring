b5da3e10e3c7603d48fb303dff256235b3120ca8
Samarth Tanay <samarth27tanay17122004@gmail.com>
2025-04-22 13:52:28 +0530

    Removed obsolete backend for Update profile functionality

--- updateProfile/updateProfileRoutes.js
+++
@@ -1,9 +0,0 @@
-// updateProfile/updateProfileRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getProfile, updateProfile } = require('./updateProfileController');
-
-router.get('/', getProfile);        // GET /api/profile?email=...
-router.put('/', updateProfile);     // PUT /api/profile
-
-module.exports = router;
