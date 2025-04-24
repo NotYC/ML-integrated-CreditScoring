01e125d528d293d9f5d18fa08b3a607b4cbea2ef
agnishwarr <agnishwar.raychaudhuri22@st.niituniversity.in>
2025-04-24 08:52:09 +0530

    Deleted old credit_scoring app

--- credit_scoring_app/config.py
+++
@@ -1,5 +0,0 @@
-# Configuration file for the credit scoring app
-
-MONGO_URI = "mongodb://localhost:27017/"  # Change if using a cloud database like MongoDB Atlas
-DATABASE_NAME = "credit_scoring_db"
-COLLECTION_NAME = "users"
