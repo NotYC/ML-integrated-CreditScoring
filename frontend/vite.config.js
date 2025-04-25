811c60775d97125b05f1d8f81a361c615a49d7c8
gautam343 <gautam.sharma22@st.niituniversity.in>
2025-04-25 19:17:23 +0530

    vite.config for docker (5174 because 5173 was docker deamon occupied)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     react(),
     tailwindcss()],
   server:{
-    port: portNumber,
-    host: 'IP for FrontEnd'
+    port: 5174,
+    host: true,
+    strictPort: true,
   }
 })
